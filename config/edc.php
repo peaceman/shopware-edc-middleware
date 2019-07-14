ed87024a08e86d28bbb82d4c27b3f064b4ccbbbe
peaceman <nixalio@gmail.com>
2019-07-14 19:48:20 +0200

    Add app specific config to env

--- config/edc.php
+++ config/edc.php
@@ -4,9 +4,9 @@ use App\EDCFeed;
 
 return [
     'feedURI' => [
-        EDCFeed::TYPE_DISCOUNTS => 'https://plx-fill.me',
-        EDCFeed::TYPE_PRODUCTS => 'https://plx-fill.me',
-        EDCFeed::TYPE_PRODUCT_STOCKS => 'https://plx-fill.me',
+        EDCFeed::TYPE_DISCOUNTS => env('EDC_FEED_DISCOUNTS', 'https://plx-fill.me'),
+        EDCFeed::TYPE_PRODUCTS => env('EDC_FEED_PRODUCTS', 'https://plx-fill.me'),
+        EDCFeed::TYPE_PRODUCT_STOCKS => env('EDC_FEED_PRODUCT_STOCKS', 'https://plx-fill.me'),
     ],
-    'imageBaseURI' => 'https://plx-fill.me/500'
+    'imageBaseURI' => env('EDC_IMAGE_BASE_URI', 'https://plx-fill.me/500'),
 ];
