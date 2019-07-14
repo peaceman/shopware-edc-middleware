11d3fce0d21e725e9da973f3f5175b9274128f44
peaceman <nixalio@gmail.com>
2019-07-14 19:45:06 +0200

    Add edc:fetch-feed command

--- app/EDC/Import/EDCImportServiceProvider.php
+++ app/EDC/Import/EDCImportServiceProvider.php
@@ -12,7 +12,9 @@ use App\EDC\Import\Jobs\ParseProductFeed;
 use App\EDC\Import\Jobs\ParseProductStockFeed;
 use App\EDCFeed;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Bus\Dispatcher as JobDispatcher;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Foundation\Console\Kernel;
 use Illuminate\Support\ServiceProvider;
 
 class EDCImportServiceProvider extends ServiceProvider
@@ -40,17 +42,23 @@ class EDCImportServiceProvider extends ServiceProvider
     {
         $this->registerParseFeedJobs();
         $this->registerProductImageLoader();
+
+        if ($this->app->runningInConsole()) $this->registerCommands();
     }
 
-    protected function registerParseFeedJobs()
+    protected function registerCommands()
     {
-        $this->app->extend(DispatchParseFeed::class, function (DispatchParseFeed $dispatchParseFeed) {
-            foreach ($this->parseFeedJobMapping as $feedType => $jobClass) {
-                $dispatchParseFeed->registerParseFeedJob($feedType, $jobClass);
-            }
+        /** @var Kernel $consoleKernel */
+        $consoleKernel = $this->app[Kernel::class];
 
-            return $dispatchParseFeed;
-        });
+        $consoleKernel
+            ->command(
+                'edc:fetch-feed {type : discounts, products, product-stocks}',
+                function (JobDispatcher $jobDispatcher, string $type) {
+                    $jobDispatcher->dispatch(new FetchFeed($type));
+                }
+            )
+            ->describe('Dispatch job to fetch the specified edc feed');
     }
 
     protected function scheduleJobs()
@@ -62,6 +70,17 @@ class EDCImportServiceProvider extends ServiceProvider
             $schedule->job(new FetchFeed(EDCFeed::TYPE_DISCOUNTS))->dailyAt('04:23');
             $schedule->job(new FetchFeed(EDCFeed::TYPE_PRODUCTS))->cron('23 5 */3 * *');
             $schedule->job(new FetchFeed(EDCFeed::TYPE_PRODUCT_STOCKS))->hourlyAt(5);
+        });
+    }
+
+    protected function registerParseFeedJobs()
+    {
+        $this->app->extend(DispatchParseFeed::class, function (DispatchParseFeed $dispatchParseFeed) {
+            foreach ($this->parseFeedJobMapping as $feedType => $jobClass) {
+                $dispatchParseFeed->registerParseFeedJob($feedType, $jobClass);
+            }
+
+            return $dispatchParseFeed;
         });
     }
 
