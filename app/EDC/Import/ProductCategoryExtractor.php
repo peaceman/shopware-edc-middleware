a153a153725f96b68455190866026b793a8818a9
peaceman <nixalio@gmail.com>
2019-07-24 17:11:03 +0200

    ProductCategoryExtractor: add logging and only extract categories of products that occurred in the last feed

--- app/EDC/Import/ProductCategoryExtractor.php
+++ app/EDC/Import/ProductCategoryExtractor.php
@@ -5,29 +5,50 @@
 
 namespace App\EDC\Import;
 
+use App\EDCFeed;
 use App\EDCProduct;
 use App\ResourceFile\StorageDirector;
+use Illuminate\Database\Eloquent\Builder;
+use Psr\Log\LoggerInterface;
 
 class ProductCategoryExtractor
 {
     /** @var StorageDirector */
     protected $storageDirector;
 
-    public function __construct(StorageDirector $storageDirector)
+    /** @var LoggerInterface */
+    protected $logger;
+
+    public function __construct(StorageDirector $storageDirector, LoggerInterface $logger)
     {
         $this->storageDirector = $storageDirector;
+        $this->logger = $logger;
     }
 
     public function extract(): array
     {
         $categories = [];
 
+        $latestProductFeed = EDCFeed::withType(EDCFeed::TYPE_PRODUCTS)->latest()->first();
+
+//        $query = EDCProduct::query();
+        $query = EDCProduct::query()
+            ->whereHas('currentData.feedPartProduct', function (Builder $q) use ($latestProductFeed) {
+                $q->where('full_feed_id', $latestProductFeed->id);
+            });
+
         /** @var EDCProduct $edcProduct */
-        foreach (EDCProduct::query()->cursor() as $edcProduct) {
+        foreach ($query->cursor() as $edcProduct) {
             $productCategories = $this->extractCategoriesFromProduct($edcProduct);
 
             foreach ($productCategories as $productCategory) {
                 [$mainCategory, $subCategory] = $productCategory;
+
+                $this->logger->info('CategoryExtractor', [
+                    'edcProduct' => $edcProduct->asLoggingContext(),
+                    'main' => $mainCategory,
+                    'sub' => $subCategory,
+                ]);
 
                 $mainCategory['childs'][$subCategory['id']] = $subCategory;
 
